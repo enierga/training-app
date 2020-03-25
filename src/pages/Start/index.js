f2a9f7c9324271d140a034ea7c0c31366318dd52
Jairo-Rubio <jairorub@live.unc.edu>
2020-03-25 16:11:05 -0400

    figured out how to go to next page, see App.js and Home

---
+++ src/pages/Start/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Jumbotron, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+
+class Start extends Component{
+    render(){
+        return(<container>
+            <h1>Module Start!
+            </h1>
+        </container>);
+    }
+}
+
+export default Start;

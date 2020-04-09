b6ad3b80b09f97fb32c9daad606d8210eead8cb8
Jolie Lau <lau20c@live.unc.edu>
2020-04-09 17:05:37 -0400

    collection routes done

--- src/App.js
+++ src/App.js
@@ -3,10 +3,25 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './pages/01-Home/home'
 import Policy from './pages/02-Start/1-Policy';
 import Purpose from './pages/02-Start/2-Purpose';
+
 import Solids from './pages/03-Types/1-Solids'
 import Liquids from './pages/03-Types/2-Liquids'
 import Sharps from './pages/03-Types/3-Sharps'
 import Path from './pages/03-Types/4-Pathological'
+
+import Primary from './pages/04-Collection/1-Solids/1-Primary'
+import Secondary from './pages/04-Collection/1-Solids/2-Secondary'
+import SolidsUnacc from './pages/04-Collection/1-Solids/3-Unacceptable'
+import TipsA from './pages/04-Collection/2-Pipettes/1A-Tips'
+import TipsB from './pages/04-Collection/2-Pipettes/1B-Tips'
+import PlasticA from './pages/04-Collection/2-Pipettes/2A-Plastic'
+import PlasticB from './pages/04-Collection/2-Pipettes/2B-Plastic'
+import LiqGen from './pages/04-Collection/3-Liquids/1-General'
+import Flask from './pages/04-Collection/3-Liquids/2-Flask'
+import LiqUnacc from './pages/04-Collection/3-Liquids/3-Unacceptable'
+import SharpsGen from './pages/04-Collection/4-Sharps/1-General'
+import Contam from './pages/04-Collection/4-Sharps/2-Contaminated'
+import SharpsUnacc from './pages/04-Collection/4-Sharps/3-Unacceptable'
 
 import DisposalStart from './pages/05-Disposal/Start'
 import ProcessA from './pages/05-Disposal/1A-Process'
@@ -34,16 +49,8 @@ class App extends Component {
   render() {
     return (
 
-      /*
-      * I think we should route different pages here and other pages
-      * can be added/routed as theyre created.
-      *  - Vincent <3
-      */
-
-
       <div className='App' >
         <Router>
-          {/* this is how itll know what page to render */}
           <Switch>
             {/*01-Home*/}
             <Route exact path="/" component={Home} />
@@ -56,7 +63,20 @@ class App extends Component {
             <Route exact path="/Liquids" component={Liquids} />
             <Route exact path="/Sharps" component={Sharps} />
             <Route exact path="/Path" component={Path} />
-
+            {/* 04-Collection */}
+            <Route exact path="/Primary" component={Primary} />
+            <Route exact path="/Secondary" component={Secondary} />
+            <Route exact path="/SolidsUnacc" component={SolidsUnacc} />
+            <Route exact path="/TipsA" component={TipsA} />
+            <Route exact path="/TipsB" component={TipsB} />
+            <Route exact path="/PlasticA" component={PlasticA} />
+            <Route exact path="/PlasticB" component={PlasticB} />
+            <Route exact path="/LiqGen" component={LiqGen} />
+            <Route exact path="/Flask" component={Flask} />
+            <Route exact path="/LiqUnacc" component={LiqUnacc} />
+            <Route exact path="/SharpsGen" component={SharpsGen} />
+            <Route exact path="/Contam" component={Contam} />
+            <Route exact path="/SharpsUnacc" component={SharpsUnacc} />
             {/*05-Disposal*/}
             <Route exact path="/DisposalStart" component={DisposalStart} />
             <Route exact path="/ProcessA" component={ProcessA} />
@@ -68,7 +88,6 @@ class App extends Component {
             <Route exact path="/Carc" component={Carc} />
             <Route exact path="/DPolicy" component={DPolicy} />
             <Route exact path="/Injury" component={Injury} />
-
             {/* End */}
             <Route exact path="/End" component={End} />
 

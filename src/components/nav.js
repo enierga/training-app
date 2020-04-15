c94273870f9815e7f3a11ac04bfa1dfa5f985e12
Vincent Enierga <venierga@email.unc.edu>
2020-04-15 01:24:57 -0400

    created countdown on next button for text only pages

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button} from 'react-bootstrap';
+import { Button, Spinner } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -13,23 +13,37 @@ export default class Nav extends Component {
 
     componentDidMount() {
         /*
-        * I only set this timeout to be seconds here but copy this structure 
+        * I only set this timeout to be 5 seconds here but copy this structure 
         * and we can figure out how long to make the wait for each page later
         */
-        setTimeout(() => this.setState({ isButtonDisabled: false}), 5000);
+        var time = 5000;
+        setTimeout(() => this.setState({ isButtonDisabled: false }), time + 1000);
+
+        const timer = setInterval(function () {
+            document.querySelector('#timeLeft').textContent = (time / 1000) + " ";
+            if (time <= 0) {
+                document.querySelector('#nextButton').textContent = "NEXT";
+                clearInterval(timer);
+            }
+            time -= 1000;
+        }, 1000)
     }
-    
-    render () {
+
+
+
+    render() {
         return (
             <div>
                 <Link to={`/${this.props.prev}`}>
-                    <Button variant='warning' style={{color: 'white'}}>PREVIOUS</Button>
+                    <Button variant='warning' style={{ color: 'white' }}>PREVIOUS</Button>
                 </Link>
-                {/* <Link to={`/${this.props.next}`}>
-                    <Button variant='warning' disabled={this.state.isButtonDisabled} style={{color: 'white', float: "right"}}>NEXT</Button>
-                </Link> */}
+
                 <Link to={`/${this.props.next}`}>
-                    <Button variant='warning' style={{color: 'white', float: "right"}}>NEXT</Button>
+                    <Button id="nextButton" variant='warning' disabled={this.state.isButtonDisabled} style={{ color: 'white', float: "right" }}>
+                        <Spinner animation="border" variant="info" size="sm"/> 
+                        <span id="timeLeft"></span>
+                         seconds left
+                         </Button>
                 </Link>
             </div>
         )

0463fb8032d0cf2ebb3bbf4885d0054a35d26dcc
Austin Joiner <aujoiner@live.unc.edu>
2020-04-21 17:34:05 -0400

    New Stuff

---
+++ src/pages/03-Types/FTB.js
@@ -0,0 +1,91 @@
+import React, { Component, useState, useCallback } from 'react';
+import { Container, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import Header from '../../components/header';
+import Card from './Card'
+import update from 'immutability-helper'
+const style = {
+  width: 400,
+}
+
+
+
+const FTB = () => {
+    {
+      
+      const [state, setState] = useState({
+        correctOrder : false,
+      })
+      const checkOrder = (cards) =>{
+        // console.log(cards)
+          if (document.getElementById("answer").value != 'EHS'){ 
+            alert("Incorrect Answer...")
+            console.log(state.correctOrder)
+          }else if(document.getElementById("answer").value == 'EHS'){
+            
+            alert("Correct Answer!")
+            
+            setState(
+              update(state, {
+                correctOrder : {
+                  $set : true
+                }
+              }),
+            )
+            console.log(state.correctOrder)
+            
+          }
+        
+      }
+      
+      
+      
+      
+
+      console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+      console.log(state.correctOrder);
+      return state.correctOrder ? (
+        <>
+            <Container>
+            <Header/>
+            <h1>Module Comprehension Fill in the Blank Question</h1>
+            <p>
+            You completed the question.
+            </p>
+            
+            <br/><br/><br/>
+            <Link to="/BucketSampDnD">
+                <Button variant='warning' style={{color: 'white'}}>PREVIOUS</Button>
+            </Link>
+            {/*Correct the link and add route*/}
+            <Link to="/">
+                    <Button variant='warning' style={{color: 'white', float: "right"}}>NEXT</Button>
+            </Link>
+            </Container>
+        </>
+      ):(
+        <>
+            <Container>
+            <Header/>
+            <h1>Module Comprehension Fill in the Blank Question</h1>
+            <p>
+            Biological waste contaminated with hazardous chemicals, radioactive substances, or carcinogens require special disposal by ______________. 
+            </p>
+            <input defaultValue = "Enter Answer Here" id = "answer">
+            </input> <br></br><br></br>
+            <button className = "enter" onClick={checkOrder}>
+                Enter
+            </button>
+            
+            <br></br><br></br>
+            <Link to="/BucketSampDnD">
+                <Button variant='warning' style={{color: 'white'}}>PREVIOUS</Button>
+            </Link>
+            </Container>
+        </>
+      )
+    }
+  }
+
+export default FTB
---
+++ src/pages/03-Types/bucketdnd copy 2.js
@@ -0,0 +1,167 @@
+import React, { Component, useState, useCallback } from 'react';
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import uuid from "uuid/v4";
+import Header from '../../components/header';
+import { Container, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import update from 'immutability-helper'
+
+const itemsFromBackend = [
+  { id: uuid(), content: "Contaminated Pippette Tips" },
+  { id: uuid(), content: "Bacterial Plates" },
+  { id: uuid(), content: "Contaminated Media Plates" }
+];
+
+const columnsFromBackend = {
+  [uuid()]: {
+    name: "Waste",
+    items: itemsFromBackend
+  },
+  [uuid()]: {
+    name: "Biohazard Waste",
+    items: []
+  },
+  [uuid()]: {
+    name: "Glass",
+    items: []
+  },
+  [uuid()]: {
+    name: "Exposed Sharps",
+    items: []
+  },
+  [uuid()]: {
+    name: "Trash Bin",
+    items: []
+  }
+};
+
+const onDragEnd = (result, columns, setColumns) => {
+  if (!result.destination) return;
+  const { source, destination } = result;
+
+  if (source.droppableId !== destination.droppableId) {
+    const sourceColumn = columns[source.droppableId];
+    const destColumn = columns[destination.droppableId];
+    const sourceItems = [...sourceColumn.items];
+    const destItems = [...destColumn.items];
+    const [removed] = sourceItems.splice(source.index, 1);
+    destItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...sourceColumn,
+        items: sourceItems
+      },
+      [destination.droppableId]: {
+        ...destColumn,
+        items: destItems
+      }
+    });
+  } else {
+    const column = columns[source.droppableId];
+    const copiedItems = [...column.items];
+    const [removed] = copiedItems.splice(source.index, 1);
+    copiedItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...column,
+        items: copiedItems
+      }
+    });
+  }
+};
+
+function BucketSampDnD3() {
+  const [columns, setColumns] = useState(columnsFromBackend);
+  return (
+    <div>
+      <Header/>
+        <p align="center">Bloodborne Pathogen Comprehension Drag and Drop Question #3:  Put the items in the correct containers.</p>
+        <div style={{ display: "flex", justifyContent: "center", height: "100%" }}>
+      
+        <DragDropContext
+          onDragEnd={result => onDragEnd(result, columns, setColumns)}
+        >
+          {Object.entries(columns).map(([columnId, column], index) => {
+            return (
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center"
+                }}
+                key={columnId}
+              >
+                <h2>{column.name}</h2>
+                <div style={{ margin: 8 }}>
+                  <Droppable droppableId={columnId} key={columnId}>
+                    {(provided, snapshot) => {
+                      return (
+                        <div
+                          {...provided.droppableProps}
+                          ref={provided.innerRef}
+                          style={{
+                            background: snapshot.isDraggingOver
+                              ? "lightblue"
+                              : "lightgrey",
+                            padding: 4,
+                            width: 250,
+                            minHeight: 500
+                          }}
+                        >
+                          {column.items.map((item, index) => {
+                            return (
+                              <Draggable
+                                key={item.id}
+                                draggableId={item.id}
+                                index={index}
+                              >
+                                {(provided, snapshot) => {
+                                  return (
+                                    <div
+                                      ref={provided.innerRef}
+                                      {...provided.draggableProps}
+                                      {...provided.dragHandleProps}
+                                      style={{
+                                        userSelect: "none",
+                                        padding: 16,
+                                        margin: "0 0 8px 0",
+                                        minHeight: "50px",
+                                        backgroundColor: snapshot.isDragging
+                                          ? "#263B4A"
+                                          : "#456C86",
+                                        color: "white",
+                                        ...provided.draggableProps.style
+                                      }}
+                                    >
+                                      {item.content}
+                                    </div>
+                                  );
+                                }}
+                              </Draggable>
+                            );
+                          })}
+                          {provided.placeholder}
+                        </div>
+                      );
+                    }}
+                  </Droppable>
+                </div>
+              </div>
+          );
+        })}
+      </DragDropContext>
+      <Link to="/SortingSampDnD2">
+        <Button variant='warning' style={{color: 'white'}}>PREVIOUS</Button>
+      </Link><br></br><br></br>
+      <Link to="/BucketSampDnd4">
+        <Button variant='warning' style={{color: 'white'}}>NEXT</Button>
+      </Link>
+    </div>
+    </div>
+  );
+}
+
+export default BucketSampDnD3;
